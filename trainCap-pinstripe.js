828b71cb9d5fc44e1a360f1a1443f1add305f3ff
Rob J. Caskey <robjcaskey@gmail.com>
2014-11-28 14:20:54 +0000

    actually serve the static files for the web page

--- trainCap-pinstripe.js
+++ trainCap-pinstripe.js
@@ -1,7 +1,10 @@
 var express = require('express')
 var app = express()
-app.get('/', function(rq, res) {
+app.set('view engine', 'jade')
+app.use('/bower_components', express.static(__dirname + '/bower_components')); 
+app.use('/js', express.static(__dirname + '/js')); 
+app.use('/trainControlImages', express.static(__dirname + '/trainControlImages')); 
+app.get('/', function(req, res) {
   res.render('trainControls.jade', { layout: false });
 });
-var server = app.listen(8080, function() {
-});
+app.listen(8080)
